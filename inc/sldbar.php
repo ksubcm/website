582f759935d84d77271cefd67bdf7e9404e3fbef
theNerd247 <harvey.coder247@gmail.com>
2014-02-11 18:21:42 -0500

    fixed the arrows on the carousel

--- inc/sldbar.php
+++ inc/sldbar.php
@@ -18,12 +18,13 @@ class Slideshow
 		global $conf;//grab the global configuration (see conf/config.php)
 		echo "<div class='hidden-xs'>
 		
-		<div id='myCarousel' class='carousel slide'>
+		<div id='myCarousel' class='carousel slide' data-ride='carousel'>
 		<div class='carousel-inner'>";
 		/* print the image containers */
 		$active = False;
 		foreach($this->paths as $path)
 		{
+			/** make the first slide the active slide */
 			if(! $active)
 			{
 				echo "<div class='item active'><img src=".$conf['IMGPATH']."/".$path." class='img-responsive'></div>";
@@ -35,9 +36,8 @@ class Slideshow
 		/* print the next and previous buttons */
 		echo "
 		</div><!-- /carousel inner -->
-				<a class='left carousel-control'  data-slide='prev' href='#myCarousel'><</a>
-				<a class='right carousel-control' data-slide='next' href='#myCarousel'>></a>
-			
+				<a class='left carousel-control'  data-slide='prev' href='#myCarousel'><span class='glyphicon glyphicon-chevron-left'></span></a> 
+				<a class='right carousel-control' data-slide='next' href='#myCarousel'><span class='glyphicon glyphicon-chevron-right'></span></a>
 		</div></div><!-- /myCoursel -->";
 	}
 }
