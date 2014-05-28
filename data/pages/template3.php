2811b2e97fe4a32b4bf13fb909c58a73664e6109
theNerd247 <harvey.coder247@gmail.com>
2014-05-28 12:51:40 -0400

    removed old pages, replacing with templates

--- data/pages/template3.php
+++ data/pages/template3.php
@@ -8,25 +8,34 @@
 
 <div class="container">
 
-<div style="height: 250; width: 100%;"><img class="full" src="data/img/imghere.png"/></div>
+<div style="height: 250; width: 100%;">
+	<img class="full" src="data/img/imghere.png"/>
+</div>
 
+<div style="margin-bottom: 1em;">
 <h1>Title</h1>
 <div>
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.
 </div>
+</div>
 
-<br>
-<span style="display: inline-block" class="pull-right">
-<h1 style="display: inline;">Title</h1>
+<div class="pull-right" style="width: 60%">
+	<h1 style="display: inline;">Title</h1>
 	<img class="pull-left" style="margin-right: 1em;" src="data/img/imghere.png"/>
-	<div style="width: 75%; vertical-align: top;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam
+	<div>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam
 lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec
-consectetur ante hendrerit.</div>
-</span>
+consectetur ante hendrerit.
+	</div>
+</div>
+<div class="clearer"></div>
 
-<h1>Title</h1>
 <div>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.
+	<h1>Title</h1>
+	<div>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.
+	</div>
 </div>
 
 </div> <!-- end containter -->
+
