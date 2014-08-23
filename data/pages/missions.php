5f961784df973ba8790e252b3e9595c68322fd4b
theNerd247 <harvey.coder247@gmail.com>
2014-08-23 15:10:01 -0400

    pre hackathon

--- data/pages/missions.php
+++ data/pages/missions.php
@@ -1,57 +1,73 @@
 
-		<div class="editable" style=""><div class="jumbotron jumbotron-title jumbotron-yellow">
+	
+	
+	
+	
+	
+	
+	
+	
+	
+			<div class="editable" style=""><div class="jumbotron jumbotron-title jumbotron-yellow">
 <div class="container">
-<h1>Our Missions</h1>
+<h1>Missions</h1>
 
 <p>"And He said to them, “Go into all the world and preach the gospel to all creation." Mark 16:15</p>
 </div>
 </div>
 
 <div class="container">
-<div class="tmove-left">
-<h1>Title</h1>
+<div style="margin-bottom: 1em;">
+<h1><img alt="" src="data/img/imghere.png" style="float:right; height:209px; margin-left:1em; width:215px">Missions at BCM</h1>
+
+<div>
+<p>KSU-SPSU BCM aims to advance the kingdom of God on our campuses and in our community. To do this we gather together for mission trips as well as raising funds for our Send Me Now summer missionaries. We would love for you to join us this year as we serve others, check our <a href="main.php?pageid=events">events page </a>for upcoming mission trips. Each year our missions team sets a fundraising goal to fund our summary missionaries. If you would like to donate, or help us in providing free mission trips for those who are willing to go please <a href="main.php?pageid=contactus">contact us</a>.</p>
+
+<div class="clearer">&nbsp;</div>
 </div>
 
-<div class="tble">
-<div class="tcell tmove-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</div>
+<div>
+<h1 style="text-align:right"><img alt="" src="data/img/imghere.png" style="float:left; height:209px; margin-right:1em; width:215px">Send Me Now</h1>
 
-<div class="tcell tfill-right"><img class="pull-left timage" src="data/img/imghere.png"></div>
-</div>
-&nbsp;
+<p>Send Me Now is a missions organization that helps students engage in mission trips in Georgia, the United States, and all over the world. To find out more about Send Me Now and to check for other mission opportunities, visit <a href="http://sendmenow.net/">their website</a> for more details.</p>
 
-<table style="width:100%" border="0" cellpadding="1" cellspacing="1">
-	<tbody>
-		<tr>
-			<th colspan="2" style="text-align:center">
-			<h1>Table Header</h1>
-			</th>
-		</tr>
-		<tr>
-			<td>
-			<h3>Header 1</h3>
-
-			<p>Vivamus fermentum semper porta. Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis ullamcorper ultricies. Curabitur ornare, ligula semper consectetur sagittis, nisi diam iaculis velit, id fringilla sem nunc vel mi. Nam dictum, odio nec pretium volutpat, arcu ante placerat erat, non tristique elit urna et turpis. Quisque mi metus, ornare sit amet fermentum et, tincidunt et orci. Fusce eget orci a orci congue vestibulum.</p>
-			</td>
-			<td>
-			<h3>Header 2</h3>
-
-			<p>Vivamus fermentum semper porta. Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis ullamcorper ultricies. Curabitur ornare, ligula semper consectetur sagittis, nisi diam iaculis velit, id fringilla sem nunc vel mi. Nam dictum, odio nec pretium volutpat, arcu ante placerat erat, non tristique elit urna et turpis. Quisque mi metus, ornare sit amet fermentum et, tincidunt et orci. Fusce eget orci a orci congue vestibulum.</p>
-			</td>
-		</tr>
-	</tbody>
-</table>
-
-<div class="tmove-right">
-<h1>Title</h1>
+<p>&nbsp;</p>
 </div>
 
-<div class="tble">
-<div class="tcell tfill-left"><img class="pull-right timage" src="data/img/imghere.png"></div>
+<h1>&nbsp;</h1>
 
-<div class="tcell tmove-right">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</div>
+<h1>Community Service<img alt="" src="data/img/imghere.png" style="float:right; height:209px; margin-left:1em; width:215px"></h1>
+
+<p>We desire to impact the community around us through our local outreach projects. We partner with local organizations and churches to reach and serve those who are in need around us. Past projects we have been involved in include: <a href="http://www.booksforafrica.org/">Books for Africa</a>, visiting <a href="http://rossmemorial.com/">Ross Memorial Nursing Home</a>, hosting a block party at Sedgefield Apartment homes, assisting at Camino de Vida Kid Zone and English classes, and being involved in <a href="http://www.handsonatlanta.org/Mission_and_Values">Hands on Atlanta </a>at Hollydale Elementary School.&nbsp;</p>
+
+<p>Serving others in the community is a great way to get to know those in our community as well as each other. We always have plenty of community service opportunities. Stay tuned for more service projects coming up on our events page, and feel free to come serve with us when you can!</p>
+</div>
+<!-- end containter --></div>
+<!-- /page_content --><!-- /CONTENT --><!-- ******************** FOOTER ******************** --><!-- LOGIN MODAL -->
+
+<div class="modal fade" id="modal-id">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header">×
+<h4>Login to BCM</h4>
+</div>
+
+<div class="modal-body">
+<form action="" method="POST" name="login_form"><input name="act" value="uid" type="hidden">
+<p><input name="uid" value="" type="text"></p>
+
+<p><input name="passwd" type="password"></p>
+
+<div class="form-actions">Sign in</div>
+</form>
 </div>
 </div>
-<!-- end containter --></div><!-- /page_content -->
+</div>
+<!-- /.modal-content --></div>
+<!-- /.modal-dialog -->
+
+<p>&nbsp;</p>
+</div><!-- /page_content -->
 <!-- /CONTENT -->
 
 <!-- ******************** FOOTER ******************** -->
--- data/pages/worship.php
+++ data/pages/worship.php
@@ -1,8 +1,6 @@
-
-	
 		<div class="editable" style=""><div class="jumbotron jumbotron-title jumbotron-purple">
 <div class="container">
-<h1>Our Worship</h1>
+<h1>Worship</h1>
 
 <p>" make my joy complete by being of the same mind, maintaining the same love, united in spirit, intent on one purpose. Do nothing from selfishness or empty conceit, but with humility of mind regard one another as more important than yourselves; do not <em>merely</em> look out for your own personal interests, but also for the interests of others. Have this attitude in yourselves which was also in Christ Jesus" Phil 2:3-5</p>
 </div>
@@ -12,20 +10,32 @@
 <div style="height: 250; width: 100%;"><img class="full" src="data/img/imghere.png"></div>
 
 <div style="margin-bottom: 1em;">
-<h1>Title<img alt="" src="data/img/imghere.png" style="float:right; height:209px; margin-left:1em; width:215px"></h1>
-
-<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</div>
+<h1>What is Worship at BCM?<img alt="" src="data/img/imghere.png" style="float:right; height:209px; margin-left:1em; width:215px"></h1>
 
 <div>
-<h1 style="text-align:right">Title<img alt="" src="data/img/imghere.png" style="float:left; height:209px; margin-right:1em; width:215px"></h1>
+<p>Our worship team at BCM desires to give God all the glory, honor, and praise He deserves by leading others to His presence through praise and worship. We desire to continue to fulfill the calling of this scripture by worshiping the Lord with humility, passion, and intimacy- and leading others to do the same.</p>
 
-<p style="text-align:right">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</p>
+<p>&nbsp;</p>
+</div>
+
+<div>
+<h1 style="text-align:right">Fusion<img alt="" src="data/img/imghere.png" style="float:left; height:209px; margin-right:1em; width:215px"></h1>
+
+<p>We gather weekly at <a href="http://www.kfbc.org/">Kennesaw First Baptist Church </a>on Tuesdays at 8:00pm. We would love for you to join us this year as we gather&nbsp; to give praise and glory to our heavenly Father! If you have any questions regarding times or transportation please do no hesitate to <a href="main.php?pageid=contactus">contact us</a>. Checkout our <a href="main.php?pageid=events">events page</a> for other great things that are going on this year!</p>
+
+<p>&nbsp;</p>
+</div>
+
+<div>
+<h1>What We Have Done<img alt="" src="data/img/imghere.png" style="float:right; height:209px; margin-left:1em; width:215px"></h1>
+
+<p>The past year we have held a fall worship night called Harvest Worship Night. It was a great time to give thanks and worship God in song and fellowship as the fall was coming to a close.</p>
+
+<p>&nbsp;</p>
 </div>
 </div>
 
-<div class="clearer">&nbsp;</div>
-
-<div class="clearer">&nbsp;</div>
+<div class="clearer">on</div>
 </div>
 <!-- end containter -->
 
