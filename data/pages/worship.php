79886e526188dce98ce38dd740799554bfebfc37
theNerd247 <harvey.coder247@gmail.com>
2014-11-04 10:30:51 -0500

    finished worhsip page

--- data/pages/worship.php
+++ data/pages/worship.php
@@ -1,22 +1,13 @@
 <div class="jumbotron jumbotron-title jumbotron-purple">
 	<div class="container">
 		<h1>Worship</h1>
-		<p>" make my joy complete by being of the same mind, maintaining the same love, united in spirit, intent on one purpose. Do nothing from selfishness or empty conceit, but with humility of mind regard one another as more important than yourselves; do not <em>merely</em> look out for your own personal interests, but also for the interests of others. Have this attitude in yourselves which was also in Christ Jesus" Phil 2:3-5</p>
+		<p>
+			"All the nations you have made will come and worship before you, O Lord; they will bring glory to your name.  For you are great and do marvelous deeds; you alone are God." Ps 86:9-10
+		</p>
 	</div>
 </div>
 
 <div class="container">
-<div class="row padrow">
-	<h1>How Do We Worship?</h1>
-	<p>
-		Our worship team at BCM desires to give God all the glory, honor, and praise
-		He deserves by leading others to His presence through song and service. We
-		desire to continue to fulfill the calling of this scripture by worshiping the
-		Lord with humility, passion, and intimacy and leading others to do the
-		same.
-	</p>
-</div>
-
 <div class="row">
 	<div class="col-sm-4">
 		<div class="row padrow">
@@ -24,7 +15,15 @@
 		</div>
 		<div class="row padrow">
 			<h2>Fusion</h2>
-			<p>We gather weekly at <a href="http://www.kfbc.org/">Kennesaw First Baptist Church </a>on Tuesdays at 8:00pm. We would love for you to join us this year as we gather&nbsp; to give praise and glory to our heavenly Father! If you have any questions regarding times or transportation please do no hesitate to <a href="main.php?pageid=contactus">contact us</a>. Checkout our <a href="main.php?pageid=events">events page</a> for other great things that are going on this year!</p>
+			<p>
+				We gather weekly at <a href="http://www.kfbc.org/">Kennesaw First
+				Baptist Church </a>on Tuesdays at 8:00pm. We would love for you to join us this
+				year as we gather&nbsp; to give praise and glory to our heavenly Father! If you
+				have any questions regarding times or transportation please do no hesitate to <a
+				href="main.php?pageid=contactus">contact us</a>.  Checkout the other <a
+				href="main.php?pageid=events">events</a> that are
+				going on this year!
+			</p>
 		</div>
 	</div>
 	<div class="col-sm-4">
@@ -34,10 +33,12 @@
 		<div class="row padrow">
 			<h2>Confluence</h2>
 			<p>
-				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-				tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-				vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-				no sea takimata sanctus est Lorem ipsum dolor sit amet.
+				Confluence is a Georgia collegiate conference. All BCMs in
+				Georgia come together for worship, challenging messages, and amazing
+				campus training. Come to be challenged to grow in your walk with Christ
+				and to learn new ways of reaching your campus! For more information
+				please visit the 
+				<a href=""http://confluencega.com/ target="_blank">Confluence website</a>.
 			</p>
 		</div>
 	</div>
@@ -48,10 +49,12 @@
 		<div class="row padrow">
 			<h2>Join our Worship team</h2>
 			<p>
-				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-				tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-				vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-				no sea takimata sanctus est Lorem ipsum dolor sit amet.
+					Our worship team at BCM desires to give God all the glory, honor, and
+					praise He deserves by leading others to His presence through song and
+					service. If you have a musical or technical talent we would love for
+					you to be a part of our worship team! Please <a
+					href="main.php?pageid=contactus" target="_blank">contact</a> our
+					worship leader if you are interested.
 			</p>
 		</div>
 	</div>
