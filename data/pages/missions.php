c623aa98eec4c6bda216290c22e394c95aba3159
theNerd247 <harvey.coder247@gmail.com>
2014-11-10 22:27:20 -0500

    almost done missions page

--- data/pages/missions.php
+++ data/pages/missions.php
@@ -1,49 +1,61 @@
 <div class="jumbotron jumbotron-title jumbotron-yellow">
 	<div class="container">
-	<h1>Missions</h1>
-		<p>"And He said to them, “Go into all the world and preach the gospel to all creation." Mark 16:15</p>
+		<h1>Missions</h1>
+		<p>
+			"And He said to them, “Go into all the world and preach the gospel to all creation." Mark 16:15
+		</p>
 	</div>
 </div>
 
 <div class="container">
-	<div>
-		<h1>
-			<img alt="" src="data/img/imghere.png" style="float:right; height:209px; margin-left:1em; width:215px">
-			Missions at BCM
-		</h1>
-	<div>
-	<p>
-		KSU-SPSU BCM aims to advance the kingdom of God on our campuses and in our
-		community. To do this we gather together for mission trips as well as raising
-		funds for our Send Me Now summer missionaries. We would love for you to join us
-		this year as we serve others, check our <a href="main.php?pageid=events">events
-		page </a>for upcoming mission trips. Each year our missions team sets a
-		fundraising goal to fund our summary missionaries. If you would like to donate,
-		or help us in providing free mission trips for those who are willing to go
-		please <a href="main.php?pageid=contactus">contact us	</a>.
-	</p>
-	<div class="clearer"></div>
+<div class="row">
+	<div class="col-xs-6">
+		<h1>Missions At KSU BCM</h1>
+		<p>
+			KSU-SPSU BCM aims to advance the kingdom of God on our campuses and in our
+			community. To do this we gather together for mission trips as well as raising
+			funds for our Send Me Now summer missionaries. We would love for you to join us
+			this year as we serve others, check our events page for upcoming mission trips.
+			Each year our missions team sets a fundraising goal to fund our summary
+			missionaries. If you would like to donate, or help us in providing free mission
+			trips for those who are willing to go please contact us.
+		</p>
+	</div>
+	<div class="col-sm-6">
+		<h1><img alt="" class="circle center-block" src="data/img/imghere.png" style="height:280px; width:280px"></h1>
+	</div>
 </div>
 
-<div>
-<h1 style="text-align:right">
-	<img alt="" src="data/img/imghere.png" style="float:left; height:209px; margin-right:1em; width:215px">
-	Send Me Now
-</h1>
-
-<p>
-	Send Me Now is a missions organization that helps students engage in mission
-	trips in Georgia, the United States, and all over the world. To find out more
-	about Send Me Now and to check for other mission opportunities, visit <a
-	href="http://sendmenow.net/">their website</a> for more details.
-</p>
-
+<div class="row">
+	<div class="col-xs-6">
+		<h1><img src="data/img/imghere.png" style="height:280px; width:280px" class="img-circle center-block"></h1>
+	</div>
+	<div class="col-xs-6">
+		<h1>Send Me Now</h1>
+		<p>
+			Send Me Now is a missions organization that helps students engage in
+			mission trips in Georgia, the United States, and all over the world. To find out
+			more about Send Me Now and to check for other mission opportunities, visit their
+			website for more details.
+		</p>
+	</div>
 </div>
 
-<h1>Community Service<img alt="" src="data/img/imghere.png" style="float:right; height:209px; margin-left:1em; width:215px"></h1>
-
-<p>We desire to impact the community around us through our local outreach projects. We partner with local organizations and churches to reach and serve those who are in need around us. Past projects we have been involved in include: <a href="http://www.booksforafrica.org/">Books for Africa</a>, visiting <a href="http://rossmemorial.com/">Ross Memorial Nursing Home</a>, hosting a block party at Sedgefield Apartment homes, assisting at Camino de Vida Kid Zone and English classes, and being involved in <a href="http://www.handsonatlanta.org/Mission_and_Values">Hands on Atlanta </a>at Hollydale Elementary School.&nbsp;</p>
-
-<p>Serving others in the community is a great way to get to know those in our community as well as each other. We always have plenty of community service opportunities. Stay tuned for more service projects coming up on our events page, and feel free to come serve with us when you can!</p>
+<div class="row">
+	<div class="col-xs-6">
+		<h1>Campus Outreach</h1>
+		<p>
+			Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+			tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+			vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+			no sea takimata sanctus est Lorem ipsum dolor sit amet.
+		</p>
+	</div>
+	<div class="col-sm-6">
+		<h1><img src="data/img/imghere.png" style="height:280px; width:280px" class="img-circle center-block"></h1>
+	</div>
 </div>
+
+
 </div>
+<!-- end containter -->
